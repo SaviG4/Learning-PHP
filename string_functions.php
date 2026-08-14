07e659c6a7f55cffb4867040b2faeaf9beb33072
Savi Gudka <mb011479@student.reading.ac.uk>
2026-08-14 09:38:28 +0100

    string functions

---
+++ string_functions.php
@@ -0,0 +1,34 @@
+<?php
+    $username = "Savi Gudka";
+    $phone = "123-456-7890";
+
+    // $username = strtolower($username); //all lowercase
+    // $username = strtoupper($username); //all uppercase
+    // $username = trim($username); //removes whitespaces before or after string
+    // $username = str_pad($username, 20, "xo"); //(x, y, z) fills string x with z until string is y length
+    // $phone = str_replace("-", "", $phone); //(x, y, z) replaces all x with y in string z
+    // $username = strrev($username); //reverses string
+    // $username = str_shuffle($username); //shuffles all the letters in a string randomly
+    // $equals = strcmp($username, "Savi Gudka"); //compares 2 strings (true = 0, false = -1 or 1)
+    // $count = strlen($phone); //counts number of characters
+    // $firstname = substr($username, 0, 4); //(x, y) creates a new string from index x for y characters 
+    // $surname = substr($username, 5, 5);
+
+
+    // $string_to_array = explode("h", "ahahahahahaha", 5); //(x, y, z) converts y into an array, at seperations of x, for z amounts of elements
+
+    // foreach($string_to_array as $i) {
+    //     echo "$i <br>";
+    // }
+
+
+    // $array_to_string = implode("h", array("a", "a", "a", "a", "a", "a","a")); //(x, y) converts y into a string, using x as a character to join all elements together
+
+
+    echo "$username <br>";
+    echo "$phone <br>";
+    // echo "$equals <br>";
+    // echo "$count <br>";
+    // echo "$firstname <br>";
+    // echo "$surname <br>";
+?>
